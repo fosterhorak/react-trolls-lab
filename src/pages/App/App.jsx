7718494914271f761c46666a2a32ab4d16837cf0
Foster Horak <fosterhorak@macbook-pro.attlocal.net>
2021-01-15 18:00:55 -0600

    added auth, neworder, orderhistory, and nav

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,12 +1,32 @@
 import { useState } from 'react';
 import './App.css';
+import AuthPage from '../AuthPage/AuthPage';
+import NewOrderPage from '../NewOrderPage/NewOrderPage';
+import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import NavBar from '../../components/NavBar/NavBar';
 
 export default function App() {
   const [user, setUser] = useState(null);
 
   return (
     <main className="App">
-      Hello, this is a new App
+      { user ? 
+          <>
+            <NavBar />
+            <Switch>
+              <Route path="/orders/new">
+                <NewOrderPage /> 
+              </Route>
+              <Route path="/orders">
+                <OrderHistoryPage /> 
+              </Route>
+              <Redirect to="/orders" />
+            </Switch>
+          </>
+        :
+          <AuthPage />
+      }
     </main>
   );
 }
